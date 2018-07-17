1bc08cff4fa2c63fc5ff77be1e5ee7008bc3e4fd
lopu <ireply@myleisure.com.au>
2018-07-17 13:18:53 +1000

    ready

---
+++ node/debug.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'alopu-api',
+      script    : 'node/debug.sh',
+      watch	: [ 'node/index.js', 'node/alopu.js', 'node/models', 'node/routes', 'node/scripts', 'node/'],
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+			},
+			node_args: "--harmony"
+    }
+//,
+
+    // Second application
+//    {
+//      name      : 'WEB',
+//      script    : 'web.js'
+//    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
