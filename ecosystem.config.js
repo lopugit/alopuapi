a6c61263ba007498f761928a6c84c2af97b49813
Nikolaj Frey <lopudesigns@gmail.com>
2020-08-19 16:33:53 +1000

    Modified pm2 config so dev only spawns 1 instance and has same memory restart cap as production, and removed production deploy boilerplate

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [
+		{
+			name: 'dev-alopuapi',
+			script: 'node/index.js',
+			args: '--level dev',
+			instances: 1,
+			autorestart: true,
+			watch: true,
+			max_memory_restart: '600M',
+		},
+		{
+			name: 'alopuapi',
+			script: 'node/index.js',
+			args: '--level prod',
+			instances: 4,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: '600M',
+		}
+	]
+};
